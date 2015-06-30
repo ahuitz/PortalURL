d969b1123c56985f0db5e19cbef11d8d66cca6c8
Crstndfp <thecris.danny@gmail.com>
2015-06-29 18:02:09 -0600

    Creacion de las clases no priviligiadas *	R_NoPrivilegiado *	T_NoPrivilegiado las cuales extiende de Recurso y Tarea respectivamente y la clase R_NoPrivilegiado por el momento no tiene funcion aparente

---
+++ src/cursos/R_NoPrivilegiado.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cursos;
+
+/**
+ *
+ * @author RealG4Life
+ */
+public class R_NoPrivilegiado extends Recurso{
+
+    /* esta clase es no es utilizada*/
+    
+    @Override
+    public void visualizar() {
+    }
+    
+}
---
+++ src/cursos/T_NoPrivilegiado.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cursos;
+
+/**
+ *
+ * @author RealG4Life
+ */
+public class T_NoPrivilegiado extends Tarea{
+
+    @Override
+    public void visualizar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void entregar(){
+    }
+    
+    public void modificar(){
+    }
+    
+}
