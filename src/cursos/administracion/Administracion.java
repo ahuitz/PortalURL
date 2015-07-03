a50dd92cd9ccc55b5294902b7c2c6d7ad886353e
est1573011 <est1573011@192.168.56.1>
2015-07-02 20:34:35 -0600

    Creacion de la clase administracion la cual tendra como tarea la creacion, modificacion

---
+++ src/cursos/administracion/Administracion.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cursos.administracion;
+
+import cursos.Curso;
+
+/**
+ *
+ * @author est1573011
+ */
+public class Administracion{
+
+    public Administracion() {
+    }
+    
+    // tengo que recibir un objeto curso
+    public void Crear_Curso(){
+    }
+    
+    //tengo que recibir un objeto curso
+    public void Crear_Seccion(){
+    }
+    
+}
