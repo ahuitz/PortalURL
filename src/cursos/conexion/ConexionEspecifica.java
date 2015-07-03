763bcd092643dfe2d13cca430c061aa2bec77da1
EST1629311 <EST1629311@192.168.56.1>
2015-07-02 19:51:53 -0600

    Se creo el modulo de conexion a la base de datos.

---
+++ src/cursos/conexion/ConexionEspecifica.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cursos.conexion;
+/**
+ *
+ * @author EST1629311
+ */
+public class ConexionEspecifica extends Conexion{
+    private static ConexionEspecifica unicaConexion;
+
+  
+    private ConexionEspecifica(String persistenceUnitName) {
+        iniciarConexion(persistenceUnitName);  
+    }
+    
+    public static ConexionEspecifica getConexionEspecifica(String persistenceUnitName){
+        if(unicaConexion == null){
+            unicaConexion=new ConexionEspecifica(persistenceUnitName);
+        }
+        return unicaConexion;
+    }
+}
